5c82bc7e1e22ad9c78a14abee7915792bf52f772
Peter <peterpadberg@hotmail.com>
2025-02-16 15:49:05 +0100

    Add sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
 import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
 import icon from 'astro-icon'
+import sentry from '@sentry/astro'
+import dotenv from 'dotenv'
+
+// Load the environment variables
+dotenv.config()
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   site: 'https://incluud.dev',
-  integrations: [icon(), compress()],
+  integrations: [
+    icon(),
+    compress(),
+    sentry({
+      dsn: process.env.SENTRY_DSN,
+      environment: process.env.SENTRY_ENVIRONMENT,
+      sourceMapsUploadOptions: {
+        project: process.env.SENTRY_PROJECT,
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   i18n: {
     locales: ['en', 'nl'],
     defaultLocale: 'en',
